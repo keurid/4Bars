8deaa6f4952fec7a55efeeb5c5dbce48eb040a7d
keurid <kevindesantiago99@gmail.com>
2023-10-11 19:51:28 -0700

    fixed index

--- client/src/index.js
+++ client/src/index.js
@@ -1,51 +1,11 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-const User = require('./models/userModel');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-require('dotenv').config();
-
-const app = express();
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-const JWT_SECRET = process.env.JWT_SECRET || 'your_default_secret_key';
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    const token = req.headers.authorization || '';
-
-    let user = null;
-    try {
-      if (token) {
-        user = jwt.verify(token, JWT_SECRET);
-      }
-    } catch (error) {
-      console.error('Error verifying JWT token:', error.message);
-    }
-
-    return { user };
-  },
-});
-
-server.applyMiddleware({ app });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}${server.graphqlPath}`);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
