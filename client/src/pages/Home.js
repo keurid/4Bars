52c969b9ea7a180643ba56a9309d4274eaeaf84b
Ashley Tan <ashleytan39@gmail.com>
2023-10-14 01:47:47 -0500

    styled homepage and moved music to home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+
 
 const Home = () => {
+  const headingStyle = {
+    fontFamily: 'Satisfy, cursive',
+    color: '#c5f7ff',
+  };
   return (
-    <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>Welcome to 4Bars!</p>
-
-      
+    <div style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+        <audio controls autoPlay>
+          <source src="lofi music.wav" type="audio/wav" />
+        </audio>
+      </div>
+      <div style={{ paddingTop: '50px', textAlign: 'center' }}>
+        <h2 style={headingStyle}>Welcome to the Home Page</h2>
+        <Link to="/signup" style={{ marginRight: '10px' }}>
+          <Button type="primary">Sign Up</Button>
+        </Link>
+        <Link to="/login">
+          <Button type="primary">Log In</Button>
+        </Link>
+      </div>
     </div>
   );
 };
