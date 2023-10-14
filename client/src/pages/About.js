ffb1c0d932e3637fc94aa9234b6452c80e0a6bb5
Yasmin Castillo <yasmin_c@outlook.com>
2023-10-14 10:48:42 -0500

    added search functionality

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Search from './Search';
 
 const About = () => {
   return (
     <div>
+      <Search />
       <h2>About Us</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
