b20b3a233f917f9da9405084701fe273fbc968dc
Yasmin Castillo <yasmin_c@outlook.com>
2023-10-16 21:26:34 -0500

    fetching api data

--- client/src/components/AlbumDetails/AlbumDetails.js
+++ client/src/components/AlbumDetails/AlbumDetails.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+const key = "523532";
 
-const AlbumDetails =() => {
-    
-    return(
-        <p>AlbumDetails</p>
-    )
+const AlbumDetails = () => {
+  const [albumDetails, setAlbumDetails] = useState(null);
 
+  useEffect(() => {
+    const fetchAlbumDetails = async () => {
+      try {
+        const album = localStorage.getItem("selectedAlbum");
+        const artist = localStorage.getItem("selectedArtist");
 
-}
+        const response = await fetch(
+          `https://www.theaudiodb.com/api/v1/json/${key}/searchalbum.php?s=${artist}&a=${album}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch album details");
+        }
+
+        const data = await response.json();
+
+        setAlbumDetails(data);
+
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchAlbumDetails();
+  }, []);
+
+  return (
+    <main>
+      <h1>ALBUM DETAILS</h1>
+      {/* Render album details here */}
+      {albumDetails && (
+        <div>
+          <p>Title: {albumDetails.title}</p>
+          <p>Artist: {albumDetails.artist}</p>
+          {/* Add more details as needed */}
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default AlbumDetails;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,43 +1,48 @@
-import React,{useState} from "react";
-import { useNavigation } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const key = "523532";
+
 export default function Search() {
-    const[search,setSearch] = useState("")
-    const[albums,setAlbums] = useState([])
-    const navigate = useNavigation();
-    function handleLocalStorage(id){
-        localStorage.setItem("selectedAlbum",id);
-        navigate("/album");
-    }
-    function handleSearch() {
-        console.log(search)
-        fetch("https://www.theaudiodb.com/api/v1/json/"+key+"/searchalbum.php?s=" + search)
-        .then(res =>res.json())
-        .then(data =>{
-            console.log(data)
-            //localStorage.setItem("selectedAlbum",data.album.)
-            //when album name is clicked
-            //use on click function to save album id into local storage
-            //then redirect to to album page
-            setAlbums(data.album)
+  const [search, setSearch] = useState("");
+  const [albums, setAlbums] = useState([]);
+  const navigate = useNavigate();
 
-        }).catch(err =>
-            console.log(err)) 
-    }
+  const handleLocalStorage = (id, artistId) => {
+    localStorage.setItem("selectedAlbum", id);
+    localStorage.setItem("selectedArtist", artistId);
+    navigate("/album");
+  };
 
-    return(
-        <div>
-        <input onChange={(e) =>setSearch(e.target.value.toLowerCase())} type ="text" placeholder ="Search"></input>
-        <button onClick={handleSearch}>Submit</button>
-        <div>
-            {albums.map(album => (
-                <div>
-                    <p onClick={() =>handleLocalStorage(album.idAlbum)}>{album.strAlbum}</p>
-                </div>
-            ))}
-        </div>
-        </div>
-    )
+  const handleSearch = () => {
+    console.log(search);
+    fetch(`https://www.theaudiodb.com/api/v1/json/${key}/searchalbum.php?s=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.album);
+      })
+      .catch((err) => console.log(err));
+  };
 
+  return (
+    <div>
+      <input
+        onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        type="text"
+        placeholder="Search"
+      />
+      <button onClick={handleSearch}>Submit</button>
+      <div>
+        {albums.map((album, index) => (
+          <div key={index}>
+            <p onClick={() => handleLocalStorage(album.idAlbum, album.idArtist)}>
+              {album.strAlbum}
+            </p>
+            <p>{album.idArtist}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
