3cbc253957f5332a2c8826d6b23a684d6d4a0fec
Ashley Tan <ashleytan39@gmail.com>
2023-10-17 21:15:05 -0500

    added basic playlist code

---
+++ client/src/pages/Playlist.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const playlistList = () => {
+  const headingStyle = {
+    fontFamily: "Satisfy, cursive",
+    color: "#c5f7ff",
+  };
+  const textStyle = {
+    color: "white",
+    fontFamily: "Alata, sans-serif",
+  };
+  return (
+    <div>
+      <h2 style={headingStyle}>Playlist</h2>
+      <p style={textStyle}>
+    
+      </p>
+    </div>
+  );
+};
+
+export default playlistList;
